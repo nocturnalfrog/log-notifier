7a4cdf14171da7a2c635ba4d1b6d9f09029c20c7
Tjerk Ameel <tjerk.ameel@gmail.com>
2014-07-06 15:55:50 +0200

    Updating Brew formula to v0.2

--- log-notifier.rb
+++ log-notifier.rb
@@ -1,13 +1,13 @@
-require 'formula'
+require "formula"
 
 class LogNotifier < Formula
-  homepage 'https://github.com/nocturnalfrog/log-notifier'
-  head 'https://github.com/nocturnalfrog/log-notifier.git'
-  url 'https://github.com/nocturnalfrog/log-notifier/archive/v0.1.tar.gz'
-  sha1 'dc698a295849ec4294607632ab801084027f66d2'
+  homepage "https://github.com/nocturnalfrog/log-notifier"
+  head "https://github.com/nocturnalfrog/log-notifier.git"
+  url "https://github.com/nocturnalfrog/log-notifier/archive/v0.2.tar.gz"
+  sha1 "0553dbfea9759f773d9c5407e679009a7c267948"
 
   def install
-    bin.install 'log-notifier'
+    bin.install "log-notifier"
   end
 
   # Adding dependencies
