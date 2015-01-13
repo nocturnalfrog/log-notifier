f4e6c790599c7de0f6ab4161f783ff5dff3e84dc
Tjerk Ameel <tjerk.ameel@gmail.com>
2015-01-13 20:33:34 +0100

    Correcting sha

--- log-notifier.rb
+++ log-notifier.rb
@@ -4,7 +4,7 @@ class LogNotifier < Formula
   homepage "https://github.com/nocturnalfrog/log-notifier"
   head "https://github.com/nocturnalfrog/log-notifier.git"
   url "https://github.com/nocturnalfrog/log-notifier/archive/v0.4.tar.gz"
-  sha1 "7d867897bfeb21d8cb02abf353c74a55623f0a74"
+  sha1 "6f8403d56f82250d2d920712e9efb074b852e9d0"
 
   def install
     bin.install "log-notifier"
