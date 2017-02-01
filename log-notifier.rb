9075caec40b5ec4242ee6df4af0dd737e8cb88a8
Tjerk Ameel <tjerk.ameel@gmail.com>
2017-02-01 17:27:16 -0500

    Updating sha256

--- log-notifier.rb
+++ log-notifier.rb
@@ -4,7 +4,7 @@ class LogNotifier < Formula
   homepage "https://github.com/nocturnalfrog/log-notifier"
   head "https://github.com/nocturnalfrog/log-notifier.git"
   url "https://github.com/nocturnalfrog/log-notifier/archive/v0.4.tar.gz"
-  sha256 "6f8403d56f82250d2d920712e9efb074b852e9d0"
+  sha256 "eeae5c2e1a771dc447640da7056efcf84aa194911568fe66db170dd5588a9fd4"
 
   def install
     bin.install "log-notifier"
